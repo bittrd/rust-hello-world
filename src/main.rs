e2ee7629c690cab17097876baa9284cbf6e289d0
Ryan Daniels <rdaniels6813@gmail.com>
2019-07-25 22:17:48 -0400

    chore: added code & test for coverage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {    
+    format!("Hello, world! {}", add());
+}
+
+fn add() -> i64 {
+    2+2
+}
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(),4)
+    }
 }
